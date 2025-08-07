e53a06716fc2b8a1ca23fad2edd81989438226d6
zq <2312532284@qq.com>
2025-08-07 16:57:16 +0300

    ok

--- src/build/trajectory_to_motors/CMakeFiles/trajectory_to_motors_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/trajectory_to_motors_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/trajectory_to_motors_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- src/build/trajectory_to_motors/CMakeFiles/trajectory_to_motors_uninstall.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for trajectory_to_motors_uninstall.
